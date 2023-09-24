a033754fe35a2c46dae64462c6d4e4cd8cc6742e
AigoLee <64901960+AigoLee@users.noreply.github.com>
2023-09-24 23:13:30 +0800

    Update

--- experiment1.sh
+++ experiment1.sh
@@ -3,16 +3,15 @@
 show_help(){
 	echo "${line} 参数说明:
 	[option]		[说明]
+	-t 	<任务类型>	1: 打包任务2的/var/www;	2: 登录fpt操作文件; 如果选择1，后面参数不需要写
 	-h 	<null>		帮助
 	-a 	<ftp地址>	ftp地址，i.g., localhost
 	-p 	<端口>		默认21 
 	-u 	<user>		ftp用户名
 	-w 	<pswd> 		ftp密码
 	-o 	<操作>		操作：upload, download, delete三选一 
-	-s 	<filepath>	源文件位置 e.g., /home/littsk/testdir/1.txt
-	-f	<filename>
-	-d 	<目标路径>	目的文件所在文件夹的位置 e.g., /home/littsk/testdir
-	-t 	<任务类型>	1: 打包任务2的/var/www;	2: 登录fpt并上传文件到指定目录"
+	-s 	<filepath>	待操作的文件位置 e.g., /home/littsk/desktop/1.txt
+	-d 	<目标路径>	新文件所在文件夹的位置 e.g., /home/littsk/testdir"
 }
 show_params(){
 	echo "${line} 您输入的参数为: "
@@ -38,9 +37,10 @@ operation=""
 src_path=""
 filename=""
 dst_path=""
+dirpath=""
 
 ## 接收参数
-while getopts "t:ha:p:u:w:o:s:f:d:" opt; do
+while getopts "t:ha:p:u:w:o:s:d:" opt; do
 	case $opt in
 		t) task=$OPTARG ;;
 		h) need_help="true" ;;
@@ -50,25 +50,19 @@ while getopts "t:ha:p:u:w:o:s:f:d:" opt; do
 		w) pass=$OPTARG ;;
 		o) operation=$OPTARG ;;
 		s) src_path=$OPTARG ;;
-		f) filename=$OPTARG ;;
 		d) dst_path=$OPTARG ;;
 		?) echo "unknown param" 
 			exit 1;;
 	esac
 done
 
-show_params
+# show_params
 
 ## -h 帮助
 # need_help非空则输入help
 if [ $need_help ]; then
 	show_help
 	exit 0
-fi
-# 判断-t参数
-if [ $task -ne 1 ] && [ $task -ne 2 ]; then
-	echo "${line} -t参数错误，请输出1或者2"
-	exit 2
 fi
 
 ## 任务1 打包文件
@@ -77,6 +71,7 @@ cd /var
 echo "${line} 以下文件将被打包"
 sudo -S tar -cvf backup.tar www << EOF
 1
+
 EOF
 echo "${line} 打包完毕，压缩包信息为："
 ls -l /var/backup.tar
@@ -85,10 +80,15 @@ fi
 
 ## 任务2 登录ftp并传文件
 # 判断必要的参数是否为空
-if [ -z $address ] || [ -z $user ] || [ -z $pass ] || [ -z $operation ] || [ -z $src_path ] || [ -z $filename ] || [ -z $dst_path ]; then
+if [ -z $address ] || [ -z $user ] || [ -z $pass ] || [ -z $operation ]; then
     echo "${line} 缺少必要的参数，请按照格式输入参数"
 	show_help
     exit 3
+fi
+# 判断-t参数
+if [ $task -ne 1 ] && [ $task -ne 2 ]; then
+	echo "${line} -t参数错误，请选择1或者2"
+	exit 2
 fi
 
 # 判断操作类型是否有效
@@ -96,43 +96,52 @@ if [ $operation != "upload" ] && [ $operation != "download" ] && [ $operation !=
     echo "${line} ftp操作类型错误，请输入 upload, download 或 delete"
     exit 4
 fi
+if  [ $operation == "upload" ] || [ $operation == "download" ]; then
+	if [ -z $src_path ] || [ -z $dst_path ]; then
+		echo "${line} upload操作缺少必要的参数，请按照格式输入参数"
+		exit 7
+	fi
+fi
+if [ $operation == "delete" ] && [ -z $src_path ]; then
+	echo "${line} upload操作缺少必要的参数，请按照格式输入参数"
+	exit 7
+fi
+
+echo ${operation}
+# 
+dirpath=$(dirname $src_path)
+filename=$(basename $src_path)
 
 echo "${line} 开始${operation}"
 
-# 开始操作
+# # 开始操作
+
+if [ $operation == "upload" ]; then
 ftp -n -v $address $port << EOF
 user $user $pass
-
-# 如果是上传操作，切换到本地源文件路径和远程目标文件路径，并使用 mput 命令上传多个文件或文件夹
-if [ $operation == "upload" ]; then
-	lcd $src_path
-    put $filename
-fi
-
-# # 如果是下载操作，切换到远程源文件路径和本地目标文件路径，并使用 mget 命令下载多个文件或文件夹
-# if [ $operation == "download" ]; then
-#     cd $src_path
-#     lcd $dst_path
-#     mget *
-# fi
-
-# # 如果是删除操作，切换到远程源文件路径，并使用 mdelete 命令删除多个文件或文件夹
-# if [ $operation == "delete" ]; then
-#     cd $src_path
-#     mdelete *
-# fi
-
+lcd $dirpath
+cd $dst_path
+put $filename
 quit
 EOF
-
-# 判断 ftp 命令的返回值是否为 0，如果不为 0，则表示出现错误，输出错误信息并退出脚本
-if [ $? -ne 0 ]; then
-    echo "ftp 命令执行失败，请检查参数和网络连接"
-    exit 5
 fi
 
-# 如果没有出错，则输出传输成功的信息，并显示传输的文件列表和大小
-echo "${line} ftp 命令执行成功，已完成 ${operation} 操作"
-ls -l ${src_path} | awk '{print $5, $9}'
+if [ $operation == "download" ]; then
+ftp -n -v $address $port << EOF
+user $user $pass
+cd $dirpath
+lcd $dst_path
+get $filename
+EOF
+fi
 
+if [ $operation == "delete" ]; then
+ftp -n -v $address $port << EOF
+user $user $pass
+cd $dirpath
+delete $filename
+EOF
+fi
 
+# # 如果没有出错，则输出传输成功的信息
+echo "${line} 执行结束"
